0bfac48d88557e229d926fadebe98538ac52304c
Tarek Amr <gr33ndata@yahoo.com>
2014-08-24 15:41:49 +0300

    initial phase of twitter api

---
+++ 19898/twapi.py
@@ -0,0 +1,49 @@
+import os 
+import json
+import base64
+import requests
+
+class TWAPI:
+    
+    def __init__(self, config_file='../config.yml'):
+        #print os.getcwd()
+        self.config_file = config_file
+        self.key = ''
+        self.secret = ''
+        self.bearer = ''
+        self.load_conf()
+        self.show_conf()
+
+    def load_conf(self):
+        fd = open(self.config_file, 'r')
+        self.key = fd.readline().split(':', 1)[1].strip() 
+        self.secret = fd.readline().split(':', 1)[1].strip() 
+        fd.close()
+        self.get_bearer_token()
+
+    def show_conf(self):
+        print 'Twitter Key: ' + self.key 
+        print 'Twitter Secret: ' + self.secret
+        print 'Twitter Bearer Token: ' + self.bearer
+
+    def get_bearer_token(self):
+        self.bearer = base64.b64encode('%s:%s' % (self.key, self.secret))
+
+
+    def get_(self):
+        
+        url = 'https://api.twitter.com/oauth2/token'
+        payload = {
+            'grant_type': 'client_credentials'
+        }
+        headers = {
+            'Authorization': 'Basic ' + self.bearer,
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        }
+
+        r = requests.post(url, data=payload, headers=headers)
+        print r, r.text
+
+if __name__ == '__main__':
+    tw = TWAPI()
+    tw.get_()
