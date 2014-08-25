dbe6649a0b1b21f398e93c56f342ca28d74f3ada
Tarek Amr <gr33ndata@yahoo.com>
2014-08-25 19:27:25 +0300

    Adding CLI options

--- 19898.py
+++ 19898.py
@@ -1,9 +1,22 @@
 import argparse
 from nineteen898.twapi import TWAPI
 
-def main():
+def show_version():
+    from nineteen898 import version
+    print version.__version__
+
+def get_tweets():
     tw = TWAPI(config_file='config.yml')
     tw.get_()
+
+def main():
+    parser = argparse.ArgumentParser(description='CLI tool for social media data analysis')
+    parser.add_argument('source', choices=['twitter'], default='twitter', help='Social network to get data from')
+    parser.add_argument('-q', '--query', metavar='Query', default='1984', help='Query used to get the data based on')
+    parser.add_argument('-o', '--output', metavar='OutputFile', default='output.json', help='Dump data to filename specified by OutputFile')
+    args = parser.parse_args()
+    print args
+    
 
 if __name__ == '__main__':
     main()
