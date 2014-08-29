627f33da2c03d77ab35798902be781d5bea215ea
Tarek Amr <gr33ndata@yahoo.com>
2014-08-29 13:14:51 +0300

    License edit

--- nineteen898/gpapi.py
+++ nineteen898/gpapi.py
@@ -5,4 +5,15 @@ import requests
 
 class GPAPI:
 
-    pass
+    def __init__(self, config_file='../config.yml'):
+        #print os.getcwd()
+        self.config_file = config_file
+        self.key = ''
+        self.load_conf()
+        #self.show_conf()
+
+    def load_conf(self):
+        fd = open(self.config_file, 'r')
+        conf = yaml.load(fd)
+        self.key = conf['GoogleKey']
+        fd.close()
